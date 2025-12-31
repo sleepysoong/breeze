c045e4ab7fd794ad76e4754712a5218389fa9a6f
sleepysoong <hyunnn1123@naver.com>
2025-12-31 16:27:34 +0000

    chore: update gitignore to exclude build directories

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_sleepysoong_breeze_BreezeApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.sleepysoong.breeze.BreezeApplication"
-)
-public final class _com_sleepysoong_breeze_BreezeApplication {
-}
